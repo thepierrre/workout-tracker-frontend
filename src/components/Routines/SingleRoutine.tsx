8de80d7666af00b678d550f10e27c90d05f6d21c
thepierrre <piotr.owczarczyk98@gmail.com>
2024-03-24 16:43:14 +0100

    started working on routing

---
+++ src/components/Routines/SingleRoutine.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+const SingleRoutine = () => {
+  return <Text textColor="white">bla bla bla</Text>;
+};
+
+export default SingleRoutine;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import SingleRoutine from "./components/Routines/SingleRoutine.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MemoryRouter>
     <ChakraProvider>
       <React.StrictMode>
         <App />
+        <Routes>
+          <Route path="single-routine" element={<SingleRoutine />}></Route>
+        </Routes>
       </React.StrictMode>
     </ChakraProvider>
   </MemoryRouter>
