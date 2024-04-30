184401d91ac3a414825252e844941bddfc9dd19b
thepierrre <piotr.owczarczyk98@gmail.com>
2024-04-30 21:53:48 +0200

    added the functionality to go to previous and next week in the datepicker

--- src/components/workouts/Datepicker.tsx
+++ src/components/workouts/Datepicker.tsx
@@ -1,5 +1,5 @@
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { Flex, Text, Box } from "@chakra-ui/react";
 import { setDay } from "../../features/workout/dayInCalendarSlice";
@@ -7,35 +7,68 @@ import { format } from "date-fns";
 
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
+const today = new Date();
+
+const currentDayOfWeek = today.getDay();
+const mondayOffset = (currentDayOfWeek + 6) % 7;
+const daysOfThisWeek = Array.from({ length: 7 }, (_, index) => {
+  const date = new Date(today);
+  date.setDate(today.getDate() - mondayOffset + index);
+  return date;
+});
+
+const todayMinus7 = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+const todayPlus7 = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+const daysOfLastWeek = Array.from({ length: 7 }, (_, index) => {
+  const date = new Date(todayMinus7);
+  date.setDate(todayMinus7.getDate() - mondayOffset + index);
+  return date;
+});
+
+const daysOfNextWeek = Array.from({ length: 7 }, (_, index) => {
+  const date = new Date(todayPlus7);
+  date.setDate(todayPlus7.getDate() - mondayOffset + index);
+  return date;
+});
+
 const Datepicker = () => {
+  const [displayedWeek, setDisplayedWeek] = useState<Date[]>(daysOfThisWeek);
   const chosenDay = useSelector((state: RootState) => state.chosenDay.day);
   const dispatch = useDispatch();
 
-  const today = new Date();
-
-  const currentDayOfWeek = today.getDay();
-  const mondayOffset = (currentDayOfWeek + 6) % 7;
-  const displayedDaysOfWeek = Array.from({ length: 7 }, (_, index) => {
-    const date = new Date(today);
-    date.setDate(today.getDate() - mondayOffset + index);
-    return date;
-  });
-
   const handleActiveDay = (index: number) => {
-    const selectedDate = displayedDaysOfWeek[index];
+    const selectedDate = daysOfThisWeek[index];
     const formattedDate = format(selectedDate, "dd/MM/yyyy");
     dispatch(setDay(formattedDate));
   };
 
-  const goToPreviousWeek = () => {};
-
-  const goToFollowingWeek = () => {};
+  const handleDisplayedWeek = (week: string) => {
+    if (week === "following") {
+      if (displayedWeek === daysOfThisWeek) {
+        setDisplayedWeek(daysOfNextWeek);
+      } else if (displayedWeek === daysOfLastWeek) {
+        setDisplayedWeek(daysOfThisWeek);
+      }
+    } else if (week === "previous") {
+      if (displayedWeek === daysOfThisWeek) {
+        setDisplayedWeek(daysOfLastWeek);
+      } else if (displayedWeek === daysOfNextWeek) {
+        setDisplayedWeek(daysOfThisWeek);
+      }
+    }
+  };
 
   return (
     <Flex align="center">
-      <ChevronLeftIcon boxSize={8} onClick={() => goToPreviousWeek()} />
+      {displayedWeek !== daysOfLastWeek && (
+        <ChevronLeftIcon
+          boxSize={8}
+          onClick={() => handleDisplayedWeek("previous")}
+        />
+      )}
       <Flex gap={4}>
-        {displayedDaysOfWeek.map((day, index) => (
+        {displayedWeek.map((day, index) => (
           <Flex
             key={index}
             direction="column"
@@ -64,7 +97,12 @@ const Datepicker = () => {
           </Flex>
         ))}
       </Flex>
-      <ChevronRightIcon boxSize={8} onClick={() => goToFollowingWeek()} />
+      {displayedWeek !== daysOfNextWeek && (
+        <ChevronRightIcon
+          boxSize={8}
+          onClick={() => handleDisplayedWeek("following")}
+        />
+      )}
     </Flex>
   );
 };
