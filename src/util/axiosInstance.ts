740a0886fc817f27de0e609df894088798ead78e
thepierrre <piotr.owczarczyk98@gmail.com>
2024-07-23 09:53:07 +0200

    edited axios base url

--- src/util/axiosInstance.ts
+++ src/util/axiosInstance.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "https://my-gym-tracker.work/api/",
+  baseURL: "https://api.my-gym-tracker.work/api/",
   //baseURL: "http://localhost:8080/api/",
-  baseURL:
-    "https://workout-tracker-piotr-server-82acd6a044c6.herokuapp.com/api/",
+  //baseURL: "https://workout-tracker-piotr-server-82acd6a044c6.herokuapp.com/api/",
   withCredentials: true,
 });
 
